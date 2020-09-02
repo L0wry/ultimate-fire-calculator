76ad26fd247fad95ad174f82670013abdf961cb1
L0wry <matthewlowry@hotmail.co.uk>
2020-09-02 08:35:57 +0100

    Updates

---
+++ precache-manifest.6435515b390e67f964bc1a1920f92d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559308faf7db0550798228eae4789719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c76540c5e7fb49c7dac",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "2c76540c5e7fb49c7dac",
+    "url": "/static/js/2.6c448267.chunk.js"
+  },
+  {
+    "revision": "3bd96a2c23d132d83f21fb4c14f07c04",
+    "url": "/static/js/2.6c448267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7432905288044df46cb",
+    "url": "/static/js/main.f98bd61f.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  }
+]);
