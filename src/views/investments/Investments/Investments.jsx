e2e09e2698d0acb55cd6a1a89d0766b2e9ac1bfb
L0wry <matthewlowry@hotmail.co.uk>
2020-09-05 13:35:30 +0100

    add invstments

--- src/views/investments/Investments/Investments.jsx
+++ src/views/investments/Investments/Investments.jsx
@@ -2,59 +2,213 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {
-    Box,
-    Card,
-    CardContent,
-    Typography,
-    Grid,
-    Divider
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+  Button,
+  TextField,
+  InputAdornment
 } from '@material-ui/core';
 import { BudgetContextConsumer } from '../../../context/BudgetContext';
+import { Formik, Form, useField, useFormikContext } from "formik";
+import { string, number, object } from "yup";
 
 
-const ExpenseHeaderCard = ({ className, ...rest }) => {
+const Input = ({ label, inputProps, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and alse replace ErrorMessage entirely.
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <TextField
+        label={label}
+        className="text-input"
+        variant="outlined"
+        required
+        InputProps={inputProps ? inputProps : null}
+        {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
-    return (
-        <div
-            className={clsx(className)}
-            {...rest}
-        >
-            <Card>
-                <CardContent>
 
-                    <Typography
-                        align="left"
-                        color="textPrimary"
-                        gutterBottom
-                        variant="h3"
-                    >
-                        Investments
+const AddInvestment = ({ className, ...rest }) => {
+  return (
+    <Box>
+      <Card
+        className={clsx(className)}
+        {...rest}
+      >
+        <CardContent>
+
+          <Typography
+            align="left"
+            color="textPrimary"
+            gutterBottom
+            variant="h4"
+          >
+            Add your investments
+      </Typography>
+          <Divider />
+          <Box mt={3}>
+
+            <Formik
+              initialValues={{
+                name: "",
+                currentValue: "",
+                expectedReturn: "",
+                monthlyContribution: ""
+              }}
+              validationSchema={object({
+                name: string(),
+                currentValue: number(),
+                expectedReturn: number(),
+                monthlyContribution: number()
+
+              })}
+              onSubmit={async (values, { setSubmitting, resetForm }) => {
+                console.log(values)
+                resetForm({})
+                setSubmitting(false);
+              }}
+            >
+              <Form>
+                <Grid
+                  container
+                  justify="space-evenly"
+                  spacing={1}
+                >
+                  <Grid
+                    item
+                  >
+                    <Input
+                      label="Investment Name"
+                      name="name"
+                      type="text"
+                    />
+                  </Grid>
+
+                  <Grid
+                    item
+                  >
+                    <Input
+                      label="Current Value"
+                      name="currentValue"
+                      type="text"
+                      inputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <Typography >
+                              £
+                      </Typography>
+                          </InputAdornment>)
+                      }}
+                    />
+                  </Grid>
+
+                  <Grid
+                    item
+                  >
+                    <Input
+                      label="Expected Annual Return"
+                      name="expectedReturn"
+                      type="text"
+                      inputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Typography >
+                              %
+                      </Typography>
+                          </InputAdornment>)
+                      }}
+                    />
+                  </Grid>
+
+                  <Grid
+                    item
+                  >
+                    <Input
+                      label="Monthly Contribution"
+                      name="monthlyContribution"
+                      type="text"
+                      inputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <Typography >
+                              £
+                      </Typography>
+                          </InputAdornment>)
+                      }}
+                    />
+                  </Grid>
+
+                
+                    <Button
+                      color="primary"
+                      fullWidth
+                      variant="text"
+                      type="submit">Submit</Button>
+                </Grid>
+              </Form>
+            </Formik>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box >
+  );
+};
+
+
+const InvestmentHeader = ({ className, ...rest }) => {
+
+  return (
+    <div
+      className={clsx(className)}
+      {...rest}
+    >
+      <Card>
+        <CardContent>
+
+          <Typography
+            align="left"
+            color="textPrimary"
+            gutterBottom
+            variant="h3"
+          >
+            Investments
                   </Typography>
-                    <Divider />
-                    <Box mt={3}>
-                        <BudgetContextConsumer>
-                            {budgetContext => (
-                                <Grid
-                                    container
-                                    direction="row"
-                                    justify="flex-start"
-                                    alignItems="stretch"
-                                    spacing={3}
-                                >
+          <Divider />
+          <Box mt={3}>
+            <BudgetContextConsumer>
+              {budgetContext => (
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="stretch"
+                  spacing={3}
+                >
+                  <AddInvestment />
 
-                                </Grid>
-                            )}
-                        </BudgetContextConsumer>
+                </Grid>
+              )}
+            </BudgetContextConsumer>
 
-                    </Box>
-                </CardContent>
-            </Card>
-        </div>
-    );
+          </Box>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
-ExpenseHeaderCard.propTypes = {
-    className: PropTypes.string
+InvestmentHeader.propTypes = {
+  className: PropTypes.string
 };
 
-export default ExpenseHeaderCard;
+export default InvestmentHeader;
